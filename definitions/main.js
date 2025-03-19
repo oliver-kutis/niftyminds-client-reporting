832f60ef555529682e72b15751ec4424cb3a7601
Data Nifty <data@niftyminds.cz>
2025-03-19 09:28:11 +0000

    update: testing scripts

--- definitions/main.js
+++ definitions/main.js
@@ -1,11 +1,13 @@
 const {
-  Layer0,
-  Layer1,
-  Layer2,
-  Layer3,
-// } = require("@niftyminds/dataform-client-reporting");
-} = require("index");
-const { client } = require("../includes/clientConfig");
+    Layer0,
+    Layer1,
+    Layer2,
+    Layer3
+} = require('index');
+
+// } = require('@niftyminds/dataform-client-reporting');
+
+const { client } = require('../includes/clientConfig');
 
 const layer0 = new Layer0(clientConfig = client);
 const layer1 = new Layer1(clientConfig = client);
@@ -26,12 +28,12 @@ layer2.addJoinColumnsAndCurrencyConversionCampaigns(customPlatformCaseWhen = {
             when platform_name = 'google_ads' then CONCAT(
                 '${client.name.split('_')[0]}',
                 '_',
-                lower(split(campaign_name, ' | ')[safe_offset(0)])
+                lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
         )`,
         project_id: `when platform_name = 'google_ads' then CONCAT(
             '${client.name.split('_')[0]}',
             '_',
-            lower(split(campaign_name, ' | ')[safe_offset(0)])
+            lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
         )`,
     },
     facebook: {
@@ -39,12 +41,12 @@ layer2.addJoinColumnsAndCurrencyConversionCampaigns(customPlatformCaseWhen = {
             when platform_name = 'facebook' then CONCAT(
                 '${client.name.split('_')[0]}',
                 '_',
-                lower(split(campaign_name, ' | ')[safe_offset(0)])
+                lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
             )`,
         project_id: `when platform_name = 'facebook' then CONCAT(
             '${client.name.split('_')[0]}',
             '_',
-            lower(split(campaign_name, ' | ')[safe_offset(0)])
+            lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
         )`,
     },
     bing_ads: {
@@ -52,12 +54,12 @@ layer2.addJoinColumnsAndCurrencyConversionCampaigns(customPlatformCaseWhen = {
             when platform_name = 'bing_ads' then CONCAT(
                 '${client.name.split('_')[0]}',
                 '_',
-                lower(split(campaign_name, ' | ')[safe_offset(0)])
+                lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
             )`,
         project_id: `when platform_name = 'bing_ads' then CONCAT(
             '${client.name.split('_')[0]}',
             '_',
-            lower(split(campaign_name, ' | ')[safe_offset(0)])
+            lower(split(IF(campaign_name LIKE "%myTimi%",TRIM(REPLACE(replace(campaign_name, "myTimi |", ""),"PMax:","")), campaign_name), ' | ')[safe_offset(0)])
         )`,
     },
     sklik: {
@@ -100,7 +102,14 @@ layer3.publishLayer(`
                             contains_substr(lower(campaign_name), "at") 
                             or contains_substr(lower(campaign_name), "lu")
                         ) then 'monkeymum_de' 
-                else project_name     
+                when starts_with(lower(campaign_name), "de") then 'monkeymum_de'
+                when starts_with(lower(campaign_name), "at") then 'monkeymum_de'
+                when starts_with(lower(campaign_name), "ie") then 'monkeymum_en'
+                when starts_with(lower(campaign_name), "se |") then 'monkeymum_sv'
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "dutch") then 'monkeymum_nl'
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "french") then 'monkeymum_fr'
+                when starts_with(lower(campaign_name), "3_pla") then concat('monkeymum_', if(split(lower(campaign_name), " | ")[safe_offset(2)] = 'sw', 'sv', split(lower(campaign_name), " | ")[safe_offset(2)]))
+                else if(is_project_defined = false, 'monkeymum_OTHER', project_name)     
             end as project_name,
             case 
                 when starts_with(lower(campaign_name), "de") 
@@ -108,6 +117,13 @@ layer3.publishLayer(`
                             contains_substr(lower(campaign_name), "at") 
                             or contains_substr(lower(campaign_name), "lu")
                         ) then true
+                when starts_with(lower(campaign_name), "de") then true
+                when starts_with(lower(campaign_name), "at") then true
+                when starts_with(lower(campaign_name), "ie") then true
+                when starts_with(lower(campaign_name), "se |") then true
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "dutch") then true
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "french") then true
+                when starts_with(lower(campaign_name), "3_pla") then true
                 else is_project_defined   
             end as is_project_defined,
             case 
@@ -116,9 +132,18 @@ layer3.publishLayer(`
                             contains_substr(lower(campaign_name), "at") 
                             or contains_substr(lower(campaign_name), "lu")
                         ) then 'monkeymum_de' 
-                else project_id     
+                when starts_with(lower(campaign_name), "de") then 'monkeymum_de'
+                when starts_with(lower(campaign_name), "at") then 'monkeymum_de'
+                when starts_with(lower(campaign_name), "ie") then 'monkeymum_en'
+                when starts_with(lower(campaign_name), "se |") then 'monkeymum_sv'
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "dutch") then 'monkeymum_nl'
+                when starts_with(lower(campaign_name), "be") and ends_with(lower(campaign_name), "french") then 'monkeymum_fr'
+                when starts_with(lower(campaign_name), "3_pla") then concat('monkeymum_', if(split(lower(campaign_name), " | ")[safe_offset(2)] = 'sw', 'sv', split(lower(campaign_name), " | ")[safe_offset(2)]))
+                else if(is_project_defined = false, 'monkeymum_OTHER', project_id)
             end as project_id
-        )
+        ),
+        project_name as original_project_name,
+        project_id as original_project_id,
     from 
         agg
 `);
--- includes/clientConfig.js
+++ includes/clientConfig.js
@@ -1,61 +1,24 @@
 const client = {
-//   id: "client-id",
-//   name: "client-name",
-  id: "monkeymum_com",
-  name: "monkeymum_com",
-//   inputDataGcpProject: "gcp-project-name",
-  inputDataGcpProject: null,
-  outputDataGcpProject: "",
-  ga4SourceType: "ecomm", // can be only 'ecomm' for now
-  // all currently supported platforms
-  platforms: [
-    "ga4",
-    "google_ads",
-    "sklik",
-    "facebook",
-    "bing_ads",
-    "heureka_cz",
-    "heureka_sk",
-    "cj_affil",
-  ],
-  // each project has an entry for the platform
-  //  - If project doesn't have a separate property within platform, custom logic needs to be applied to assign it
-  //      - In that case, empty array will be passed to the platform key (as in cj_affiliate case)
-  //  - In case of sklik, heureka_cz and heureka_sk (probably cj_affiliate as well), only one property_id will be present in platform data
-//   projects: {
-//     ga4: [
-//       {
-//         property_id: "123",
-//         project_id: "project-id",
-//         project_name: "project-name",
-//       },
-//       {
-//         property_id: "123",
-//         project_id: "project-id",
-//         project_name: "project-name",
-//       },
-//     ],
-//     // ...
-//     google_ads: "custom",
-//     sklik: "custom",
-//     facebook: "custom",
-//     bing_ads: "custom",
-//     heureka_cz: [
-//       {
-//         property_id: "123",
-//         project_id: "project-id",
-//         project_name: "project-name",
-//       },
-//     ],
-//     heureka_sk: [
-//       {
-//         property_id: "123",
-//         project_id: "project-id",
-//         project_name: "project-name",
-//       },
-//     ],
-//     cj_affil: "custom",
-//   },
+    id: 'monkeymum_com',
+    name: 'monkeymum_com',
+    inputDataGcpProject: 'niftyminds-client-reporting',
+    outputDataGcpProject: '',
+    ga4SourceType: 'ecomm', // can be only 'ecomm' for now
+    // all currently supported platforms
+    platforms: [
+        'ga4',
+        'google_ads',
+        'sklik',
+        'facebook',
+        'bing_ads',
+        'heureka_cz',
+        'heureka_sk',
+        'cj_affil',
+    ],
+    // each project has an entry for the platform
+    //  - If project doesn't have a separate property within platform, custom logic needs to be applied to assign it 
+    //      - In that case, empty array will be passed to the platform key (as in cj_affiliate case)
+    //  - In case of sklik, heureka_cz and heureka_sk (probably cj_affiliate as well), only one property_id will be present in platform data
     projects: {
         ga4: [
             { property_id: '327331458', project_id: 'monkeymum_cz', project_name: 'monkeymum_cz' },
@@ -64,7 +27,7 @@ const client = {
             { property_id: '327364097', project_id: 'monkeymum_es', project_name: 'monkeymum_es' },
             { property_id: '374115092', project_id: 'monkeymum_fi', project_name: 'monkeymum_fi' },
             { property_id: '327351271', project_id: 'monkeymum_fr', project_name: 'monkeymum_fr' },
-            { property_id: '348603801', project_id: 'monkeymum_gr', project_name: 'monkeymum_gr' },
+            { property_id: '348603801', project_id: 'monkeymum_gr', project_name: 'monkeymum_el' },
             { property_id: '327335909', project_id: 'monkeymum_hr', project_name: 'monkeymum_hr' },
             { property_id: '327347972', project_id: 'monkeymum_hu', project_name: 'monkeymum_hu' },
             { property_id: '327342577', project_id: 'monkeymum_it', project_name: 'monkeymum_it' },
@@ -90,6 +53,7 @@ const client = {
     }   
 };
 
+
 module.exports = {
-  client,
-};
+    client,
+}
